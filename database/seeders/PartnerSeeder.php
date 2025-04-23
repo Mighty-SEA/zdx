259b6d6632c770253a0a46cfe8460e9c2d8a3420
Hibban Habiburrahman <hibban.habiburrahman@outlook.com>
2025-04-23 13:41:41 +0700

    minor updates

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -17,7 +17,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'RANO CIPTA NUSALOG',
-                'logo' => 'partners/logos/v1qtqvlguVEYBWjdWZK6oKMNF3MyD8PgxgIk1Mpq.png',
+                'logo_path' => 'partners/logos/v1qtqvlguVEYBWjdWZK6oKMNF3MyD8PgxgIk1Mpq.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:24:14',
@@ -25,7 +25,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'PT PANDARAN',
-                'logo' => 'partners/logos/XJqxty4hT4jn28Y0ODq0ZB7ds8R2CbHGabUezpld.png',
+                'logo_path' => 'partners/logos/XJqxty4hT4jn28Y0ODq0ZB7ds8R2CbHGabUezpld.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:24:25',
@@ -33,7 +33,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'ONGKIR EXPRESS',
-                'logo' => 'partners/logos/WOnY5BpudRjRTmlzcNv214GxDfqvjzz0PeBLJc94.png',
+                'logo_path' => 'partners/logos/WOnY5BpudRjRTmlzcNv214GxDfqvjzz0PeBLJc94.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:24:33',
@@ -41,7 +41,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'S-LOG',
-                'logo' => 'partners/logos/QcCZ0DFpf9gFsdqLHBoIrpvstD9tcVBEJOIBRX0y.png',
+                'logo_path' => 'partners/logos/QcCZ0DFpf9gFsdqLHBoIrpvstD9tcVBEJOIBRX0y.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:24:42',
@@ -49,7 +49,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'BERLY CARGO',
-                'logo' => 'partners/logos/ouT3gc9MftU0CVSXIdNH7OR89C1Q8AxE0uI0QNR0.png',
+                'logo_path' => 'partners/logos/ouT3gc9MftU0CVSXIdNH7OR89C1Q8AxE0uI0QNR0.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:24:52',
@@ -57,7 +57,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'PT BAHAGIA RIZKI SELARAS',
-                'logo' => 'partners/logos/QkpyNnIjEqiLH6zlf7Y1jmn1BpPMYqz9qiDEX1cK.png',
+                'logo_path' => 'partners/logos/QkpyNnIjEqiLH6zlf7Y1jmn1BpPMYqz9qiDEX1cK.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:25:02',
@@ -65,7 +65,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'EXPRESS & LOGISTICS',
-                'logo' => 'partners/logos/XVnLgO3Cnyls51N8syKxujfXlnQWswYQjMmiLAeW.png',
+                'logo_path' => 'partners/logos/XVnLgO3Cnyls51N8syKxujfXlnQWswYQjMmiLAeW.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:25:16',
@@ -73,7 +73,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'ARTAJASA',
-                'logo' => 'partners/logos/r06p6yYLR2YXGI3ONTyzysaS1KBNN7FKczDsEcMD.png',
+                'logo_path' => 'partners/logos/r06p6yYLR2YXGI3ONTyzysaS1KBNN7FKczDsEcMD.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:25:27',
@@ -81,7 +81,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'INTEGRATED LOGISTICS SERVICES',
-                'logo' => 'partners/logos/cD8GS3P0MUrKnr1vT6xZBDDIziYqwSXZ8Z6SWbBp.png',
+                'logo_path' => 'partners/logos/cD8GS3P0MUrKnr1vT6xZBDDIziYqwSXZ8Z6SWbBp.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:25:37',
@@ -89,7 +89,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'INTAN UTAMA LOGISTIK',
-                'logo' => 'partners/logos/4uh36tiVq1X4O5c083O1rbZd5lVLQdMFwhbwAWi7.png',
+                'logo_path' => 'partners/logos/4uh36tiVq1X4O5c083O1rbZd5lVLQdMFwhbwAWi7.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:25:48',
@@ -97,7 +97,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'EXPRESS CARGO',
-                'logo' => 'partners/logos/OynioJlMy8QqU7CxmwQes2YAriDIY8eWuLFwHEOd.png',
+                'logo_path' => 'partners/logos/OynioJlMy8QqU7CxmwQes2YAriDIY8eWuLFwHEOd.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:25:56',
@@ -105,7 +105,7 @@ class PartnerSeeder extends Seeder
             [
                 'type' => 'partner',
                 'name' => 'PT BAHAGIA JAYA SEJAHTERA',
-                'logo' => 'partners/logos/izYd7PjV3q77Tlc5RXg26QVEncDLFPo2xJHZV92A.png',
+                'logo_path' => 'partners/logos/izYd7PjV3q77Tlc5RXg26QVEncDLFPo2xJHZV92A.png',
                 'status' => 'active',
                 'created_at' => '2025-04-23 02:21:57',
                 'updated_at' => '2025-04-23 02:26:04',
